1b70c1f5bf9e4ade85d9936084ad50e7ddcf8ff7
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-12-18 18:39:15 +0300

    code refactored

--- src/q2_anagram/Main.java
+++ src/q2_anagram/Main.java
@@ -1,13 +1,13 @@
 package q2_anagram;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     //get 2 inputs then compare each other and if they are anagram, return true else false.
-    // para  -  rapa  --> return true
+    // meat  -  team  --> return true
     // test  -  task  --> return false
+
 
     public static void main(String[] args) {
 
@@ -19,20 +19,20 @@ public class Main {
 
     }
 
-    public static boolean isAnagram(String input1, String input2) {
+    private static boolean isAnagram(String input1, String input2) {
 
         if (input1.length() != input2.length()) {
             return false;
         }
 
-        char[] input1Arr = convertStringToCharArray(input1);
-        char[] input2Arr = convertStringToCharArray(input2);
+        char[] arr1 = convertToCharArray(input1);
+        char[] arr2 = convertToCharArray(input2);
 
-        mergeSort(input1Arr, input1Arr.length);
-        mergeSort(input2Arr, input2Arr.length);
+        mergeSort(arr1, arr1.length);
+        mergeSort(arr2, arr2.length);
 
-        for (int i = 0; i < input1Arr.length; i++) {
-            if (input1Arr[i] != input2Arr[i]) {
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
                 return false;
             }
         }
@@ -40,43 +40,34 @@ public class Main {
         return true;
     }
 
-    public static char[] convertStringToCharArray(String data) {
-        char[] temp = new char[data.length()];
+    private static void mergeSort(char[] a, int n) {
+        if (n > 1) {
+            int mid = n / 2;
 
-        for (int i = 0; i < data.length(); i++) {
-            temp[i] = data.charAt(i);
+            char[] l = new char[mid];
+            char[] r = new char[n - mid];
+
+            for (int i = 0; i < mid; i++) {
+                l[i] = a[i];
+            }
+
+            for (int i = mid; i < n; i++) {
+                r[i - mid] = a[i];
+            }
+
+            mergeSort(l, mid);
+            mergeSort(r, n - mid);
+
+            merge(a, l, r, mid, n - mid);
+
         }
-
-        return temp;
-    }
-
-    public static void mergeSort(char[] a, int n) {
-        if (n < 2)
-            return;
-
-        int mid = n / 2;
-
-        char[] l = new char[mid];
-        char[] r = new char[n - mid];
-
-        for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
-        }
-
-        for (int i = mid; i < n; i++) {
-            r[i - mid] = a[i];
-        }
-
-        mergeSort(l, mid);
-        mergeSort(r, n - mid);
-
-        merge(a, l, r, mid, n - mid);
 
 
     }
 
     private static void merge(char[] a, char[] l, char[] r, int left, int right) {
         int i = 0, j = 0, k = 0;
+
         while (i < left && j < right) {
             if ((int) l[i] <= (int) r[j]) {
                 a[k++] = l[i++];
@@ -84,12 +75,24 @@ public class Main {
                 a[k++] = r[j++];
             }
         }
+
         while (i < left) {
             a[k++] = l[i++];
         }
         while (j < right) {
             a[k++] = r[j++];
         }
+
+
+    }
+
+    private static char[] convertToCharArray(String input) {
+        char[] arr = new char[input.length()];
+
+        for (int i = 0; i < input.length(); i++) {
+            arr[i] = input.charAt(i);
+        }
+        return arr;
     }
 
 
